01f7f29b3107afcb39ca4242381e87059cbb62c4
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-06-05 21:33:10 +0530

    created Sorted function

--- Array/isSorted.java
+++ Array/isSorted.java
@@ -1,6 +1,11 @@
 public class isSorted {
 
+                public static void isSortedArray(int[] arr){
+                                
+                }
+
                 public static void main(String[] args) {
+                                int[] arr = { 3, 4, 5, 1, 2 };
 
                 }
 }
