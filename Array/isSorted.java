37487be31da2c4724f56e52c6045505eaf39a975
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-06-06 18:25:53 +0530

    .

--- Array/isSorted.java
+++ Array/isSorted.java
@@ -1,13 +1,20 @@
 public class isSorted {
 
-                public static void isSortedArray(int[] arr){
-                                int i=0;
-                                
+                public static void isSortedArray(int[] arr) {
+                                int i = 0;
+
+                                for (int j = 0; i < arr.length; i++) {
+                                                if (arr[j - 1] > arr[j]) {
+                                                                return false;
+
+                                                } else
+                                                                return true;
+                                }
 
                 }
 
-                public static void rotateArray(int[] arr){
-                                
+                public static void rotateArray(int[] arr) {
+
                 }
 
                 public static void main(String[] args) {
