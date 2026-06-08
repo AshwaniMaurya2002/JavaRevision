a0b2c6e59e1034720a5bbb94040a9348cc94b91b
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-06-08 17:55:33 +0530

    make the function

--- Array/secondMax.java
+++ Array/secondMax.java
@@ -1,9 +1,11 @@
 public class secondMax {
 
-
-                public static void main(String[] args) {
-                                
+                public static int secondMax(int[] arr) {
                                 
                 }
-                
+
+                public static void main(String[] args) {
+
+                }
+
 }
