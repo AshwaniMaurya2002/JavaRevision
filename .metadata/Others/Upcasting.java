237f355a3063cb618060746b42268f3997be6da0
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-06-12 23:58:23 +0530

    .

--- .metadata/Others/Upcasting.java
+++ .metadata/Others/Upcasting.java
@@ -1,9 +1,5 @@
 public class Upcasting {
 
-                public static void main(String[] args) {
-
-                                System.out.println("heello");
-
-                }
+            
 
 }
---
+++ .metadata/Others/UpcastingDriver.java
@@ -0,0 +1,8 @@
+public class UpcastingDriver extends  Upcasting {
+
+
+                
+                
+
+                
+}
