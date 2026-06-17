4da250cf82dbb771b3d4239f8b7b02f901a4e091
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-06-17 21:08:43 +0530

    .

---
+++ OOps/EncapDriver.java
@@ -0,0 +1,8 @@
+public class EncapDriver {
+k                public static void main(String[] args) {
+                                Encapsulation e = new Encapsulation();
+                                e.setName("rohan");
+                                e.getName();
+
+                }
+}
---
+++ OOps/Encapsulation.java
@@ -0,0 +1,13 @@
+public class Encapsulation {
+
+                private String name;
+                // private static int no;
+
+                public String getName() {
+                                return name;
+                }
+
+                public void setName(String name) {
+                                this.name = name;
+                }
+}
