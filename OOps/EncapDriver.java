e07ad2372ddfbc0a5e3b01bf6e80ceeb6a41698b
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-06-17 21:09:19 +0530

    encapsulation class

--- OOps/EncapDriver.java
+++ OOps/EncapDriver.java
@@ -1,5 +1,5 @@
 public class EncapDriver {
-k                public static void main(String[] args) {
+                public static void main(String[] args) {
                                 Encapsulation e = new Encapsulation();
                                 e.setName("rohan");
                                 e.getName();
